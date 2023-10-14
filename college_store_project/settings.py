21758c65fdbfc47c7c86e13d02d31f213b79f60c
neethu4611 <144012407+neethu4611@users.noreply.github.com>
2023-10-14 11:15:08 +0530

    Update settings.py

--- college_store_project/settings.py
+++ college_store_project/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
-STATIC_ROOT=os.path.join(BASE_DIR,'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, '/college_store/static/')
+
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
