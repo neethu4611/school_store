3d6405ea540c6b6cbbbd262c8bf96493ed40980d
neethu4611 <144012407+neethu4611@users.noreply.github.com>
2023-10-15 17:02:16 +0530

    Update settings.py

--- college_store_project/settings.py
+++ college_store_project/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'/college_store_project/static/')]
 STATIC_ROOT = os.path.join(BASE_DIR, '/college_store_project/static/')
 
 MEDIA_URL='/media/'
