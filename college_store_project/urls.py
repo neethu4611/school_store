dee0b79a50e50ca3e4553c83bca6ab8dda9f234e
neethu4611 <144012407+neethu4611@users.noreply.github.com>
2023-10-18 12:51:05 +0530

    Update urls.py

--- college_store_project/urls.py
+++ college_store_project/urls.py
@@ -22,7 +22,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('college_app.urls')),
+    path('college_app/', include('college_app.urls')),
     path('credentials_app/', include('credentials_app.urls')),
 ]
 if settings.DEBUG:
